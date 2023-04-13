a77e82a5b3f57f7e8924445c9e23de65f1b29c0e
kayra <mgulkajyr@inbox.ru>
2023-04-13 17:28:34 +0600

    Initial Commit

---
+++ MyAppStart/MyAppStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppStartApp.swift
+//  MyAppStart
+//
+//  Created by Каира on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyAppStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
